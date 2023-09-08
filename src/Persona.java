15cb7ad5cdfae597d2c81499004768004e2668a3
Luigui_Ayala15 <142622405+LuiguiAyala15@users.noreply.github.com>
2023-09-08 13:49:09 -0500

    Add files via upload

---
+++ src/Persona.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GAMER
+ */
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    
+     public Persona(String nombre) {
+        this.nombre = nombre;
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+   
+  
+     
+     
+
+    
+}
---
+++ src/T1AYALARODRIGUEZ.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author GAMER
+ */
+public class T1AYALARODRIGUEZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         java.awt.EventQueue.invokeLater(() -> {
+            new LstPersona().setVisible(true);
+        });
+    }
+    }
+    
+
+
